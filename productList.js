db5a6928131d53746528c6989c81f3e3fc512665
Josef Glenn Guarin <Jexsof64@gmail.com>
2020-02-28 15:04:34 -0500

    Content overhaul of Product List Page

--- productList.js
+++ productList.js
@@ -1,62 +1,129 @@
-/*
-Code from advanced.js
-*/
-
-var foo = 'foo'; // Variables declared outside of any function are considered global variables.
-				 // These variables can be found on the window object.
 (function () {
-	// Any kind of function, will create a new variable scope. Variables declared in this 
-	// function will only be accesible inside this function, unless passed by reference through
-	// a function call. 
-
-	// Lower level scope will always overwrite a higher level scope.  
-	var foo = 'bar';
-	console.log(foo); // 'bar'
-	// Global Variables can still be accessed through window object 
-	console.log(window.foo); // 'foo'
-
-	// An array of Objects, similar to database records we will eventually be dealing with.
+	
 	var mockDatabase = [
-		{ _id: '123', name: 'Article 1', published: true },
-		{ _id: '583', name: 'Article 2', published: true },
-		{ _id: '954', name: 'Article 3', published: false },
-		{ _id: '384', name: 'Article 4', published: false },
-		{ _id: '183', name: 'Article 5', published: true },
-		{ _id: '007', name: 'Article 6', published: false },
-		{ _id: '304', name: 'Article 7', published: true },
-		{ _id: '729', name: 'Article 8', published: false },
-		{ _id: '734', name: 'Article 9', published: true },
+		{ _id: '1', 
+			name: 'Blizz Dagger', 
+			price: 2800, 
+			type: 'melee', 
+			category: 'knife', 
+			attack: 56,
+			accuracy: 90,
+			effect: 'Freeze (low)',
+			description: 'A dagger with a pale blue blade.',
+			img: 'img/BlizzDagger.jpg'
+		},
+		{ _id: '2',
+			name: 'Kukri',
+			price: 3600,
+			type: 'melee',
+			category: 'knife',
+			attack: 68,
+			accuracy: 96,
+			effect: 'No Effect',
+			description: 'Curved, Nealese knife also used as a tool.',
+			img: 'img/Kukri.jpg'
+		},
+		{ _id: '3',
+			name: 'Mace',
+			price: 2800,
+			type: 'melee',
+			category: 'bludgeon',
+			attack: 54,
+			accuracy: 92,
+			effect: 'No Effect',
+			description: 'A replica modeled after a medieval club.',
+			img: 'img/Mace.jpg'
+		},
+		{ _id: '4',
+			name: 'Hell Slugger',
+			price: 4000,
+			type: 'melee',
+			category: 'bludgeon',
+			attack: 64,
+			accuracy: 94,
+			effect: 'No Effect',
+			description: 'A baseball bat with nails hammered into it.',
+			img: 'img/HellSlugger.jpg'
+		},
+		{ _id: '5',
+			name: 'Tkachev',
+			price: 1400,
+			type: 'ranged',
+			category: 'pistol',
+			attack: 66,
+			accuracy: 88,
+			effect: 'No Effect',
+			description: 'Model gun typically seen in mafia films.',
+			img: 'img/Tkachev.jpg'
+		},
+		{ _id: '6',
+			name: 'Governance',
+			price: 3000,
+			type: 'ranged',
+			category: 'pistol',
+			attack: 76,
+			accuracy: 96,
+			effect: 'No Effect',
+			description: 'Model gun of a bsic high-caliber handgun.',
+			img: 'img/Governance.jpg'
+		},
+		{ _id: '7',
+			name: 'Levinson M31',
+			price: 1400,
+			type: 'ranged',
+			category: 'shotgun',
+			attack: 64,
+			accuracy: 86,
+			effect: 'No Effect',
+			description: 'Model gun based on the short-barreled shotgun.',
+			img: 'img/LevinsonM31.jpg'
+		},
+		{ _id: '8',
+			name: 'Granelli M3',
+			price: 4400,
+			type: 'ranged',
+			category: 'shotgun',
+			attack: 88,
+			accuracy: 90,
+			effect: 'No Effect',
+			description: 'Model gun based on an oversized shotgun.',
+			img: 'img/GranelliM3.jpg'
+		},
 	];
 
 	function renderList (results) {
-		var tableBody = document.querySelector('#results-table tbody');
+		var listBody = document.querySelector('.list-container');
 
-		// clear out inner HTML to get rid of any older results
-		tableBody.innerHTML = '';
-		// Map each database record to a string containing the HTML for it's row
-		var tableRows = results.map(function (result, index) {
-			return '<tr><td>' + index + '</td><td>' + result.name + '</td><td>' + 
-					result._id + '</td><td>' + result.published + '</td></tr>';
+		// Clear out the inner HTML to get rid of any older results
+		listBody.innerHTML = '';
+		
+		// Maps each database record to a string containing the HTML for it's row
+		var listItems = results.map(function (result, index) {
+			return '<div class="product-item"><img src="'+result.img+'">'+
+			'<div class="product-data"><h3>'+result.name+'</h3><div class="price">'+result.price+'</div></div></div>';
 		});
+
 		// Set the contents of the table body to the new set of rendered HTML rows
-		tableRows.forEach(function (row) {
-			tableBody.innerHTML += row; // += adds to HTML instead of overwriting it entirely.
-		});
+		listItems.forEach(function (row) {
 
-		// Lower level scope once again overwrites what's above it.
-		var foo = 'renderList';
-		console.log(foo); // 'renderList'
+			// += adds to HTML instead of overwriting it entirely
+			listBody.innerHTML += row; 
+		});
 	}
 
 	renderList(mockDatabase);
 
 	// Function to Order results list 
 	function orderBy(sortValue) {
-		// Sort method varies based on what type of value we're sorting 
-		var sortedResults = (sortValue === 'name') ? 
-			mockDatabase.sort(function (a, b) { // Strings need to be sorted in a slightly more compldex way
-				var nameA = a.name.toUpperCase(); // ignore upper and lowercase
-				var nameB = b.name.toUpperCase(); // ignore upper and lowercase
+		var sortedResults;
+	 
+		if (sortValue === 'name') 
+		{
+			sortedResults = mockDatabase.sort(function (a, b) 
+			{
+				var nameA = a.name.toUpperCase(); 
+				var nameB = b.name.toUpperCase(); 
+				
 				// Sorts alphabetically.  -1 puts it before. 1 puts it after
 				if (nameA < nameB) {
 				    return -1;
@@ -64,16 +131,22 @@ var foo = 'foo'; // Variables declared outside of any function are considered gl
 				if (nameA > nameB) {
 				    return 1;
 				}
-			}) : 
-			mockDatabase.sort(function (a, b) { // Numbers a booleans are much simpler. 
-												// Just need postive or negative number 
-				// Object properties can be accessed through a string representing their name
+			});
+		} 
+		else 
+		{
+			sortedResults = mockDatabase.sort(function (a, b) 
+			{ 
 				return a[sortValue] - b[sortValue];
 			});
+		}
+	
 		renderList(sortedResults);
 	}
+
 	// Change events trigger after the value of a form input changes
-	document.querySelector('#orderBy').addEventListener('change', function(event){
+	document.querySelector('#orderBy').addEventListener('change', function(event)
+	{
 		// Event is the JavaScript event that transpired, in our change a CHANGE event.
 		// Target is the element it was performed on, useful for when the event targets 
 		// multiple elements.
@@ -81,25 +154,60 @@ var foo = 'foo'; // Variables declared outside of any function are considered gl
 		orderBy(event.target.value);
 	});
 
-	// Function to filter out unpublished results
-	function togglePublished(showPublished) {
-		// If showPublished is TRUE, only display published results
-		// Filter will only inclue objects that return TRUE from it's query
-		var filteredResults = mockDatabase.filter(function (result) {
-			// If showPublished is TRUE, always show.
-			// Otherweise only show if published is TRUE
-			return showPublished || result.published;
+	function filterPriceRange (priceDescrip) 
+	{
+		var priceRanges = {
+			veryCheap: [1, 10000], 
+			cheap: [10001, 20000], 
+			medium: [20001, 30000],
+			expensive: [30001, 40000],
+			veryExpensive: [40001, 60000]
+		};
+		var range = priceRanges[priceDescrip];
+
+		if (!range) 
+			return renderList(mockDatabase);
+
+		var filteredResults = mockDatabase.filter(function (row) {
+			return row.price > range[0] && row.price < range[1];
 		});
 		renderList(filteredResults);
 	}
-	// Change events trigger after the value of a form input changes
-	document.querySelector('#published').addEventListener('change', function(event){
-		// in this case value is a string that we need to convert to a boolean 
-		var value = event.target.value === 'true';
-		togglePublished(value);
+
+	document.querySelector('#priceRange').addEventListener('change', function (event) {
+		filterPriceRange(event.target.value);
+	});
+
+	function filterType (type)
+	{
+		if (!type)
+			return renderList(mockDatabase);
+
+		var filteredResults = mockDatabase.filter(function (row) {
+			return row.type === type;
+		});
+		renderList(filteredResults);
+	}
+
+	document.querySelector('#type').addEventListener('change', function (event) {
+		filterType(event.target.value);
+	});
+
+	function filterCategory (category) 
+	{
+		if (!category) 
+			return renderList(mockDatabase);
+
+		var filteredResults = mockDatabase.filter(function (row) {
+			return row.category === category;
+		});
+		renderList(filteredResults);
+	}
+
+	document.querySelector('#category').addEventListener('change', function (event) {
+		filterCategory(event.target.value);
 	});
 
 
 
-})(); // Wrap entire file in self executing function. 
-      // Keeping all variables declared in this file inside a local scope.
+})();
